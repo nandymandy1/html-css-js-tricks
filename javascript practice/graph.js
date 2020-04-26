b87da5667f1782eb4c3be5d4051a8eec3154678e
Nandy <nandymandy09@hotmail.com>
2020-04-27 04:49:47 +0530

    Graph Example

--- javascript practice/graph.js
+++ javascript practice/graph.js
@@ -32,10 +32,17 @@ airports.forEach(addNode);
 routes.forEach((route) => addEdge(...route));
 
 console.log("FIRST_ADJACENCY_LIST", adjacencyList);
-console.log("___________________________________");
+console.log(`____________________________________________ \n`);
 
 // GRAPH SEARCH OR TRAVERSAL
-function bfs(start) {
+
+/**
+ * @CASE TO TRACK WHICH ONE IS MORE EFFICIENT ROUTE
+ * @DESC ALL THE POSSIBLE ROUTE BETWEEN TWO POINTS
+ * @PARAMS STARTING POINT and ENDING POINT
+ * @FX BREADTH FIRST SEARCH FUNCTION
+ */
+const bfs = (start, end) => {
   const queue = [start];
 
   // Track the value of visited airports
@@ -47,16 +54,46 @@ function bfs(start) {
     const destinations = adjacencyList.get(airport);
 
     for (const destination of destinations) {
-      if (destination === "HYD") {
+      if (destination === end) {
         console.log(`FOUND ${start}`);
       }
 
       if (!visited.has(destination)) {
         visited.add(destination);
         queue.push(destination);
+        console.log(`DESTINATION ${destination}`);
       }
     }
   }
-}
+};
 
-bfs("LKO");
+bfs("LKO", "CAL");
+console.log(`____________________________________________ \n`);
+
+/**
+ * @CASE TO TRACK WHICH ONE IS MORE QUICKEST ROUTE EXISTS OR NOT
+ * @DESC ALL THE POSSIBLE ROUTE BETWEEN TWO POINTS
+ * @PARAMS STARTING POINT and ENDING POINT
+ * @FX DEPTH FIRST SEARCH FUNCTION
+ */
+
+let steps = 0;
+const dfs = (start, end, visited = new Set()) => {
+  console.log("START ", start);
+  visited.add(start);
+  steps = steps + 1;
+
+  const destinations = adjacencyList.get(start);
+
+  for (const destination of destinations) {
+    if (destination === end) {
+      console.log(`FOUND ${end} in ${steps} steps`);
+      return;
+    }
+    if (!visited.has(destination)) {
+      dfs(destination, end, visited);
+    }
+  }
+};
+
+dfs("LKO", "HYD");
